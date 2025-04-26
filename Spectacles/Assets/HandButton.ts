f705198bb2704d5643fbe7e2d931145d85109b61
mdushkoff <mdushkoff@gmail.com>
2025-04-26 03:28:22 -0700

    Added Generic Hand Attached UI Prefab, Cleaned Up Hierarchy

--- Spectacles/Assets/HandButton.ts
+++ Spectacles/Assets/HandButton.ts
@@ -4,39 +4,53 @@ import { SIK } from "./SpectaclesInteractionKit/SIK";
 
 @component
 export class HandButton extends BaseScriptComponent {
-  @input
-  private button: PinchButton;
-  private leftHand: TrackedHand = SIK.HandInputData.getHand("left");
+  // Inputs to the script component
+  @input private chirality: boolean;  // False -> Left : True -> Right
+  @input private button: PinchButton;
+  @input private txobj: SceneObject;
 
+  // Private internal class variables
+  private leftHand: TrackedHand = SIK.HandInputData.getHand("left");
+  private rightHand: TrackedHand = SIK.HandInputData.getHand("right");
+  private hand: TrackedHand;
+
+  // When the script wakes up
   onAwake() {
+    // Choose the hand to attach to
+    this.hand = this.chirality ? this.rightHand : this.leftHand;
+
+    // Set up events
     this.createEvent("OnStartEvent").bind(this.onStart.bind(this));
     this.createEvent("UpdateEvent").bind(this.update.bind(this));
   }
 
+  // Every frame
   private update() {
-    if (this.leftHand.isTracked()) {
-      const buttonTransform = this.button.getTransform();
-      const leftHandTransform = this.leftHand.getSceneObject().getTransform();
+    // If the hand is being tracked, transform the scene object to the hand
+    if (this.hand.isTracked()) {
+      const transform = this.txobj.getTransform();
+      const handTransform = this.hand.getSceneObject().getTransform();
 
-      // Copy TRS of the button to the world coordinates of the left hand
-      buttonTransform.setWorldRotation(leftHandTransform.getWorldRotation());
-      buttonTransform.setWorldPosition(
+      // Copy TRS of the scene object to the world coordinates of the hand
+      transform.setWorldRotation(handTransform.getWorldRotation());
+      transform.setWorldPosition(
         this.leftHand
           .getPalmCenter()
-          .add(leftHandTransform.back.uniformScale(5))
-          .add(leftHandTransform.left.uniformScale(3))
+          .add(handTransform.back.uniformScale(5))
+          .add(handTransform.left.uniformScale(3))
       );
     }
   }
 
+  // On start
   private onStart() {
-    // Show the button when the left hand is seen
-    this.leftHand.onHandFound.add(() => {
+    // Show the button when the hand is seen
+    this.hand.onHandFound.add(() => {
       this.button.sceneObject.enabled = true;
     });
 
-    // Hide the button when the left hand is not seen
-    this.leftHand.onHandLost.add(() => {
+    // Hide the button when the hand is not seen
+    this.hand.onHandLost.add(() => {
       this.button.sceneObject.enabled = false;
     });
   }
